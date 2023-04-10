c5e5d53be28187b2d13cafcde4d5e6f621ac4c8a
Pichchenda <113159533+Pichchenda@users.noreply.github.com>
2023-04-10 15:39:14 +1000

    update HTML and JS - Mid Year Sales

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-//Side Men
+//Side Menu
 function sideMenuAppears (){
   document.getElementById('side-menu').classList.add('show-menu');  
 }
@@ -74,33 +74,70 @@ document.getElementById('menu-3').addEventListener('mouseleave', hideMenu);
 document.getElementById('menu-4').addEventListener('mouseleave', hideMenu);
 document.getElementById('menu-5').addEventListener('mouseleave', hideMenu);
 
-// // scroll to left or right//
+// Mid Year Sales - Slide Show
 
+const prev =  document.querySelector('#prev');
+const next = document.querySelector('#next');
+const tvScreen = document.querySelector('#tv-screen');
+const slideShows = document.querySelectorAll('.slide-show');
 
+function nextSlide() {
+  const currentSrc = tvScreen.getAttribute('src');
+  if(currentSrc === 'img/Mid\ Year\ Sales.jpeg'){
+    tvScreen.src === 'img/mid-year-sales 2.jpg'
+  } else if (currentSrc === 'img/mid-year-sales 2.jpg'){
+    tvScreen.src === 'img/mid-year-sales 3.jpg'
+  } else {
+    tvScreen.src === 'img/mid-year-sales 4.jpg'
+  }
+}
+next.addEventListener('click', nextSlide);
 
+function prevSlide (){
+  const currentSrc = tvScreen.getAttribute('src');
+  if(currentSrc === 'img/mid-year-sales 4.jpg'){
+   tvScreen.src === 'img/mid-year-sales 3.jpg'
+  } else if(currentSrc === './img/mid-year-sales 3.jpg'){
+    tvScreen.src === 'img/mid-year-sales 2.jpg'
+  } else {
+    tvScreen === 'img/Mid\ Year\ Sales.jpeg'
+  }
+}
 
+prev.addEventListener('click', prevSlide );
 
-// 
-// heartIcons.addEventListener('click',backgroundFill);
-
-const wishList = document.querySelector('#wish-list');
-const heartIcons = document.querySelectorAll('.heart-icon');
-
-for(let i = 0; i < heartIcons.length; i++){
-  const heartIcon = heartIcons[i]; 
-  function backgroundFill (){
-      heartIcon.style.fill = "red";
-    }
-  heartIcon.addEventListener('click', backgroundFill);
-  const heartCount = heartIcon.length;
-  console.log(heartCount);
+for (let i = 0; i < slideShows.length; i++) {
+  const slideShow = slideShows[i];
+  const imgSrc = slideShow.getAttribute('src');
+  
+  function changeSlide(){
+    tvScreen.src = imgSrc;
+  }
+  
+  slideShow.addEventListener('click', changeSlide);
 }
 
 
 
-// for (let i = 0; i < heartIcons.lenghth; i++) {
-//   const heartIcon = heartIcons[i];
-//   console.log(heartIcon);
-//   // const heartCount = heartIcon.length;
-//   // console.log(heartCount);
+
+
+
+
+
+// Wishlist
+
+// const wishList = document.querySelector('#wish-list');
+// const heartIcons = document.querySelectorAll('.heart-icon');
+
+// for(let i = 0; i < heartIcons.length; i++){
+//   const heartIcon = heartIcons[i]; 
+//   function backgroundFill (){
+//       heartIcon.style.fill = "red";
+//     }
+//   heartIcon.addEventListener('click', backgroundFill);
+//   wishList.textContent = heartIcon.length;
+//   console.log(wishList);
+
 // }
+
+
